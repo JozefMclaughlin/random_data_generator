b91603c3df062674d1e262d122dacf7e8d66dbf5
tech-a47 <tech-a47@Tech-A47s-MacBook-Air.local>
2019-02-19 10:27:16 +0000

    expanded commit

--- random_data.rb
+++ random_data.rb
@@ -4,5 +4,29 @@ require 'faker'
 class DataGenerator
 
 
+  def firstname
+    Faker::Name.first_name.capitalize
+  end
 
+  def lastname
+    Faker::Name.last_name.capitalize
+  end
+
+  def get_gender
+    Faker::Gender.binary_type
+    # Faker::Gender.type  --includes non-binary
+  end
+
+  def get_city
+    Faker::Address.city
+  end
+
+
+  def get_email
+    Faker::Internet.free_emil(firstname)
+  end
+
+  def get_birthday
+    Faker::Date.birthday
+  end
 end
